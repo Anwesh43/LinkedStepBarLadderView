cdacb7ac13b20a626762ecc39ceb552044b19e06
Anwesh43 <anweshthecool0@gmail.com>
2021-01-03 23:38:12 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedstepbarladderview/MainActivity.kt
+++ app/src/main/java/com/example/linkedstepbarladderview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedstepbarladderview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.stepbarladderview.StepBarLadderView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StepBarLadderView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
