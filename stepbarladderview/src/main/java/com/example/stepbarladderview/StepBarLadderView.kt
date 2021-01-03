9c841f634ee19750d3a1de0c1939970e583db883
Anwesh43 <anweshthecool0@gmail.com>
2021-01-03 21:10:46 +0530

    adding StepBarLadderView cdlass

--- stepbarladderview/src/main/java/com/example/stepbarladderview/StepBarLadderView.kt
+++ stepbarladderview/src/main/java/com/example/stepbarladderview/StepBarLadderView.kt
@@ -75,3 +75,18 @@ fun Canvas.drawSBLNode(i : Int, scale : Float, paint : Paint) {
     drawStepBarLadder(scale, w, h, paint)
 }
 
+class StepBarLadderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
