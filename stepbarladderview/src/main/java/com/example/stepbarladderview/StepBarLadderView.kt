7ca8268f20ff98b00971c5e5bba6f6c634a4afde
Anwesh43 <anweshthecool0@gmail.com>
2021-01-03 21:40:03 +0530

    adding StepBarLadder class

--- stepbarladderview/src/main/java/com/example/stepbarladderview/StepBarLadderView.kt
+++ stepbarladderview/src/main/java/com/example/stepbarladderview/StepBarLadderView.kt
@@ -168,4 +168,27 @@ class StepBarLadderView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class StepBarLadder(var i : Int) {
+
+        private var curr : SBLNode = SBLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
